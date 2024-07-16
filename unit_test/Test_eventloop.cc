b8bcdaf75e13f3e504c26ea1364d7c0aa2a8cc18
yqm-307 <979336542@qq.com>
2024-07-16 08:45:57 +0000

    超时事件测试例程

--- unit_test/Test_eventloop.cc
+++ unit_test/Test_eventloop.cc
@@ -1,6 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/included/unit_test.hpp>
+#include <bbt/base/clock/Clock.hpp>
 #include <bbt/pollevent/EventLoop.hpp>
 #include <bbt/pollevent/Event.hpp>
 
@@ -32,6 +33,40 @@ BOOST_AUTO_TEST_CASE(test_time_count)
     loop->StartLoop(EventLoopOpt::LOOP_NORMAL);
     BOOST_CHECK(count == 10);
 
+}
+
+BOOST_AUTO_TEST_CASE(t_timer_interval)
+{
+    const int timeout_ms = 1000;
+    EventLoop* loop = new EventLoop();
+    // std::vector<std::shared_ptr<Event>> events;
+    std::map<EventId, uint64_t> regist_time_map;
+    std::map<EventId, uint64_t> timeout_map;
+
+
+    for (int i = 0; i < 1000; ++i) {
+        
+        auto event = loop->CreateEvent(0, EventOpt::TIMEOUT,
+        [&timeout_map](auto event, auto){
+            timeout_map.insert(std::make_pair(event->GetEventId(), bbt::clock::gettime()));
+        });
+
+        event->StartListen(timeout_ms);
+        // events.push_back(event);
+        regist_time_map.insert(std::make_pair(event->GetEventId(), bbt::clock::gettime()));
+    }
+
+    auto max_end_ts = bbt::clock::nowAfter(bbt::clock::seconds(2));
+    while (!bbt::clock::is_expired<bbt::clock::milliseconds>(max_end_ts))
+    {
+        loop->StartLoop(EventLoopOpt::LOOP_NONBLOCK);
+        std::this_thread::sleep_for(bbt::clock::milliseconds(10));
+    }
+
+    for (auto it : regist_time_map) {
+        auto diff = timeout_map[it.first] - it.second;
+        BOOST_CHECK_MESSAGE(diff >= timeout_ms, "diff=" << diff);
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
